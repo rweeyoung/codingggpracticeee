a0e9cd87d579375fda9480d2e598f45ab1266051
woon rwee young <woonrweeyoung@gmail.com>
2024-04-25 13:34:06 +0800

    established

---
+++ money_manager.py
@@ -0,0 +1,50 @@
+#this scripthas some error handling issues
+#simple comment
+#simple comment
+#simple comment
+#simple comment
+
+from packaged_classes import Tracker, Trackertwo
+
+first_input = None
+second_input = None
+
+
+
+while True:
+    first_input = input("press 1 to make new tracker, press 2 to access existing tracker, press 3 to exit")
+    try:
+        first_input = int(first_input)
+        if 1 > first_input or first_input > 3:
+            print("input number within range")
+            print("\n" * 3)
+            continue
+    except ValueError:
+        print("input a number")
+        print("\n" * 3)
+        continue
+    else:
+        break
+
+if first_input == 1:
+    print("making new tracker for you now")
+    x = input("what is the new name of your file: ").lower().strip()
+    new_tracker = Tracker(x)
+    new_tracker.pathtwo()
+
+if first_input == 2:
+    y = input("what is the existing name of your file").lower().strip()
+    ytracked = Trackertwo(y)
+    ytracked.pathtwo()
+
+
+print("program finished running ")
+
+
+
+
+#simple comment
+#simple comment
+#simple comment
+#simple comment
+
---
+++ packaged_classes/__init__.py
@@ -0,0 +1,16 @@
+#simple comment
+#simple comment
+#simple comment
+#simple comment
+
+from .tracker import Tracker
+from .trackertwo import Trackertwo
+
+__all__ = ["Tracker","Trackertwo","csv"]
+
+print("successfully imported two relevant classes")
+
+#simple comment
+#simple comment
+#simple comment
+#simple comment
